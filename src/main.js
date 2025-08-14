d084223ffe18020d440bab672240e8cd188ffb07
zhfrann <muhzhafranilham13@gmail.com>
2025-08-14 09:08:06 +0700

    update express to listen bost ipv4 & ipv6 for internal railway connection

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { env } from "./application/env.js";
 import { logger } from "./application/logging.js";
 import { web } from "./application/web.js";
 
-web.listen(env.port, () => {
+web.listen(env.port, "::", () => {
     logger.info(`App start on port ${env.port}`);
 });
