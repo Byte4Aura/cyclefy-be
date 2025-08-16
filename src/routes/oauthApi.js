0ed94a0ac1c955c426696a561f48264b1cc117a7
zhfrann <muhzhafranilham13@gmail.com>
2025-08-16 14:58:22 +0700

    feat(api): authentication using facebook (not work yet lol) & twitter

--- src/routes/oauthApi.js
+++ src/routes/oauthApi.js
@@ -30,6 +30,53 @@ oauthRouter.get('/api/auth/google/callback',
     }
 );
 
+// Redirect to Facebook
+oauthRouter.get('/api/auth/facebook', passport.authenticate("facebook", {
+    scope: ['email']
+}));
 
+// Callback from Facebook
+oauthRouter.get('/api/auth/google/callback',
+    passport.authenticate("facebook", { failureRedirect: '/login' }),
+    (req, res) => {
+        // Login success using facebook
+        const token = generateJWT(req.user);
+        res.status(201).json({
+            success: true,
+            message: "Authentication with Facebook successful",
+            data: {
+                id: req.user.id,
+                fullname: req.user.fullname,
+                username: req.user.username,
+                email: req.user.email,
+                profile_picture: req.user.profile_picture
+            },
+            token: token,
+        });
+    }
+);
+
+// Redirect to X (Twitter)
+oauthRouter.get('/api/auth/twitter', passport.authenticate("twitter"));
+
+// Callback from Twitter
+oauthRouter.get('/api/auth/twitter/callback',
+    passport.authenticate("twitter", { failureRedirect: '/login' }),
+    (req, res) => {
+        const token = generateJWT(req.user);
+        res.status(201).json({
+            success: true,
+            message: "Authentication with Twitter successful",
+            data: {
+                id: req.user.id,
+                fullname: req.user.fullname,
+                username: req.user.username,
+                email: req.user.email,
+                profile_picture: req.user.profile_picture
+            },
+            token: token,
+        });
+    }
+);
 
 export { oauthRouter }
