fe00d6f8f3a6c12ac2dba05b1eb963122756f459
zhfrann <muhzhafranilham13@gmail.com>
2025-09-04 21:51:19 +0700

    stuff again

--- src/services/repairService.js
+++ src/services/repairService.js
@@ -419,7 +419,8 @@ export const requestRepairPayment = async (userId, repairId, paymentType, option
 
     const midtransAuth = {
         username: env.midtrans.serverKey,
-        password: "Basic TWlkLXNlcnZlci12VEVWOTJHNlVuZzlMZXEta0Y4YUJQdkw6"
+        // password: "Basic TWlkLXNlcnZlci12VEVWOTJHNlVuZzlMZXEta0Y4YUJQdkw6"
+        password: ""
     };
 
     // 3. Request ke Midtrans Core API
