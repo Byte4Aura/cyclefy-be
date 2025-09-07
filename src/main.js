6941c71e99dd326533cd05031d44d391bbec9c2a
zhfrann <muhzhafranilham13@gmail.com>
2025-09-08 02:36:16 +0700

    feat: set cron job to be executed every 24 hours

--- src/main.js
+++ src/main.js
@@ -9,7 +9,18 @@ web.listen(env.port, "::", () => {
 });
 
 // Jalankan setiap 1 jam
-cron.schedule("0 0 * * * *", async () => {
+// cron.schedule("0 0 * * * *", async () => {
+//     try {
+//         await checkAndUpdateOverdueBorrows();
+//         console.log("Checked overdue borrows at", new Date());
+//     } catch (err) {
+//         // Handle error, bisa log ke file
+//         console.error("Error checking overdue borrows:", err);
+//     }
+// });
+
+// Jalankan setiap 24 jam (00:00:00)
+cron.schedule("0 0 0 * * *", async () => {
     try {
         await checkAndUpdateOverdueBorrows();
         console.log("Checked overdue borrows at", new Date());
